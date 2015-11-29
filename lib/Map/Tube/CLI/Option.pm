34d01d072d3f9545611027ee851594680f1e8540
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-11-29 01:50:42 +0000

    - Added key 'provides' to the Makefile.PL script.

--- lib/Map/Tube/CLI/Option.pm
+++ lib/Map/Tube/CLI/Option.pm
@@ -1,6 +1,7 @@
 package Map::Tube::CLI::Option;
 
-$Map::Tube::CLI::Option::VERSION = '0.15';
+$Map::Tube::CLI::Option::VERSION   = '0.16';
+$Map::Tube::CLI::Option::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
 
@@ -8,7 +9,7 @@ Map::Tube::CLI::Option - Option as Moo Role for Map::Tube::CLI.
 
 =head1 VERSION
 
-Version 0.15
+Version 0.16
 
 =cut
 
