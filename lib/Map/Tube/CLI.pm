779ae0f2224816ce4688e60cf3464d7ffe6b3ada
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-10-27 10:28:34 +0100

    - Upgraded to use Map::Tube::Exception v3.08.

--- lib/Map/Tube/CLI.pm
+++ lib/Map/Tube/CLI.pm
@@ -1,6 +1,6 @@
 package Map::Tube::CLI;
 
-$Map::Tube::CLI::VERSION   = '0.19';
+$Map::Tube::CLI::VERSION   = '0.20';
 $Map::Tube::CLI::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Map::Tube::CLI - Command Line Interface for Map::Tube::* map.
 
 =head1 VERSION
 
-Version 0.19
+Version 0.20
 
 =cut
 
--- lib/Map/Tube/CLI/Option.pm
+++ lib/Map/Tube/CLI/Option.pm
@@ -1,6 +1,6 @@
 package Map::Tube::CLI::Option;
 
-$Map::Tube::CLI::Option::VERSION   = '0.19';
+$Map::Tube::CLI::Option::VERSION   = '0.20';
 $Map::Tube::CLI::Option::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Map::Tube::CLI::Option - Option as Moo Role for Map::Tube::CLI.
 
 =head1 VERSION
 
-Version 0.19
+Version 0.20
 
 =cut
 
