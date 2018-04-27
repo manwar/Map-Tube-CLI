1a39bed22927fcce60c553847c1c318e278d1d11
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2018-04-27 16:13:18 +0100

    - Upgraded dependency on Map::Tube v3.50

--- lib/Map/Tube/CLI.pm
+++ lib/Map/Tube/CLI.pm
@@ -1,6 +1,6 @@
 package Map::Tube::CLI;
 
-$Map::Tube::CLI::VERSION   = '0.45';
+$Map::Tube::CLI::VERSION   = '0.46';
 $Map::Tube::CLI::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Map::Tube::CLI - Command Line Interface for Map::Tube::* map.
 
 =head1 VERSION
 
-Version 0.45
+Version 0.46
 
 =cut
 
